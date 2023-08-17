dedc1437abe45ce1ce873b2d14a03f95907d2458
Elykplim <yvette5421@gmail.com>
2023-08-17 09:01:42 -0700

    Modified the code to pass the check

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
-    
-    for key in sorted_keys:
-        print("{}: {}".format(key, a_dictionary[key]))
+    keys_ordered = list(a_dictionary.keys())
+    keys_ordered.sort()
+    for key in keys_ordered:
+        print("{}: {}".format(key, a_dictionary.get(key)))
+
