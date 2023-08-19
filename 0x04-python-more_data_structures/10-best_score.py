248b60b4592014fda7a0abd2b9f9174997db2fae
Elykplim <yvette5421@gmail.com>
2023-08-18 21:06:26 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
-
-    return (max(a_dictionary, key=a_dictionary.get))
+    if a_dictionary is None:
+        return None
+    
+    best_key = None
+    best_value = float('-inf')  # Start with negative infinity as the initial best value
+    
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_key = key
+            best_value = value
+    
+    return best_key
 
 
