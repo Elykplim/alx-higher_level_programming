457b2ac3bfb31a5e726ba04010f8853af7c933ba
Elykplim <yvette5421@gmail.com>
2023-08-18 21:34:20 -0700

    rewrote the best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or not a_dictionary:
-        return None
-    
-    best_key = None
-    best_value = float('-inf')
-    
-    for key, value in a_dictionary.items():
-        if isinstance(value, int) and value > best_value:
-            best_key = key
-            best_value = value
-    
-    return best_key
+  """
+  Returns a key with the biggest integer value.
+
+  Args:
+    a_dictionary: A dictionary where the keys are strings and the values are integers.
+
+  Returns:
+    A key with the biggest integer value, or None if no scores are found.
+  """
+
+  if a_dictionary is None:
+    return None
+
+  best_key = None
+  best_score = None
+  for key, value in a_dictionary.items():
+    if best_score is None or value > best_score:
+      best_key = key
+      best_score = value
+
+  return best_key
 
 
