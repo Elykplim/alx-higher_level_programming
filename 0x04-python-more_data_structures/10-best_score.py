1123567fbb09a993d3a5511373544cff683d6bcf
Elykplim <yvette5421@gmail.com>
2023-08-18 21:29:29 -0700

    updated best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,14 @@ def best_score(a_dictionary):
     if a_dictionary is None or not a_dictionary:
         return None
     
-    best_key = max(a_dictionary, key=a_dictionary.get)
+    best_key = None
+    best_value = float('-inf')
+    
+    for key, value in a_dictionary.items():
+        if isinstance(value, int) and value > best_value:
+            best_key = key
+            best_value = value
+    
     return best_key
 
 
